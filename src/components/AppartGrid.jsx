afb0646dc48eeb43df2f3a398ca46087e4c9493d
Slakyis <lafarguekevin@yahoo.com>
2023-07-16 06:10:13 +0200

    Bis sans notes

--- src/components/AppartGrid.jsx
+++ src/components/AppartGrid.jsx
@@ -1,37 +1,3 @@
-// import React, { useEffect, useState } from 'react';
-// import "./AppartGrid.css";
-// import AppartementCard from "./AppartementCard.jsx";
-
-// function AppartGrid() {
-//   const [appartement, setAppartement] = useState([]);
-
-//   useEffect(fetchAppartement, []);
-
-//   function fetchAppartement() {
-//     fetch("logements.json")
-//       .then((res) => res.json())
-//       .then((res) => setAppartement(res))
-//       .catch(console.error);
-//   }
-
-//   return (
-//     <div className='AppartGrid'>
-//       {appartement.map((appartement) => (
-//         <AppartementCard 
-//           key={appartement.id}
-//           title={appartement.title} 
-//           imageUrl={appartement.cover}
-//           id={appartement.id}
-//         />
-//       ))}
-//     </div>
-//   );
-// }
-
-// export default AppartGrid;
-
-// AppartementPage.jsx
-
 import React, { useEffect, useState } from 'react';
 import { useAppartementNavigate } from '../routes/router.jsx';
 import "./AppartGrid.css";
--- src/components/AppartementCard.jsx
+++ src/components/AppartementCard.jsx
@@ -1,22 +1,3 @@
-// import React from 'react'
-// import "./AppartementCard.css"
-// import { Link } from 'react-router-dom'
-
-// function AppartementCard(props) {
-//   return (
-//     <Link to="/appartement" 
-//     state={{appartementId: props.id}}>
-//       <div className='appartement__card'>
-//         <img src = {props.imageUrl} alt="" />
-//         <div className='appartement__subtitle'>{props.title}</div>
-//       </div>
-//     </Link>
-//   )
-// }
-
-// export default AppartementCard
-
-
 import React from 'react';
 import { useAppartementNavigate } from '../routes/router.jsx';
 import "./AppartementCard.css";
