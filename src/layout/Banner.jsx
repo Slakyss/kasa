db99aff2ddca9f1407887c5485b888c36437dc58
Slakyis <lafarguekevin@yahoo.com>
2023-07-26 07:25:48 +0200

    Amelioration 2/2

--- src/layout/Banner.jsx
+++ src/layout/Banner.jsx
@@ -1,30 +1,30 @@
-// import React from "react";
-// import "./Banner.css";
-
-// const Banner = ({ image, text, descriptionImg, showOverlay }) => {
-//   return (
-//     <div className='banner'>
-//       <img src={image} className='bannerImg' alt={descriptionImg} />
-//       {showOverlay && <div className="overlay"></div>}
-//       <div className="content">
-//         {text && <h3>{text}</h3>}
-//         {descriptionImg && <p>{descriptionImg}</p>}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Banner;
-
 import React from "react";
-import './Banner.css'
+import "./Banner.css";
 
-function Banner({children}) { 
-    return (
-        <div>
-            {children}
-        </div>
-    )
-}
+const Banner = ({ image, text, descriptionText, showOverlay }) => {
+  return (
+    <div className='banner'>
+      <img src={image} className='bannerImg' alt={descriptionText} />
+      {showOverlay && <div className="overlay"></div>}
+      <div className="content">
+        {text && <h3>{text}</h3>}
+        {descriptionText && <p>{descriptionText}</p>}
+      </div>
+    </div>
+  );
+};
 
-export default Banner
+export default Banner;
+
+// import React from "react";
+// import './Banner.css'
+
+// function Banner({children}) { 
+//     return (
+//         <div>
+//             {children}
+//         </div>
+//     )
+// }
+
+// export default Banner
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,19 +1,44 @@
+// import React from "react";
+// import Banner from "../layout/Banner";
+// import "./HomePage.css";
+// import AppartGrid from "../components/AppartGrid.jsx";
+// import Main from "../components/Main";
+
+// function HomePage() {
+//   return (
+//     <>
+//       <Main>
+//         <Banner>
+//           <div className="banner__home banner__all">
+//             <h1>Chez vous, partout et ailleurs</h1>
+//           </div>
+//         </Banner>         
+//         <AppartGrid />
+//       </Main>
+//     </>
+//   );
+// }
+
+// export default HomePage;
+
 import React from "react";
 import Banner from "../layout/Banner";
 import "./HomePage.css";
-import AppartGrid from "../components/AppartGrid.jsx";
+import HomeBannerImg from "../images/BannerHome.png";
 import Main from "../components/Main";
+import AppartGrid from "../components/AppartGrid.jsx";
 
 function HomePage() {
   return (
     <>
       <Main>
-        <Banner>
-          <div className="banner__home banner__all">
-            <h1>Chez vous, partout et ailleurs</h1>
-          </div>
-        </Banner>         
-        <AppartGrid />
+        <Banner className="home__banner"
+          image={HomeBannerImg}
+          text= "Chez vous, partout et ailleurs"
+          descriptionImg="Image de la bannière d'accueil"
+          showOverlay={true}
+        />
+        <AppartGrid /> 
       </Main>
     </>
   );
